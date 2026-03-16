7d1c0adbfbb754efacc04f7c8cdfc2e6ed42b895
Prasanna Kumar Vanga <113172515+vangaprasannakumar@users.noreply.github.com>
2026-03-16 11:48:33 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'capco-hrms-v2';
+
+// Install the Service Worker and cache the core files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+// Intercept network requests and serve from cache if offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Clean up old caches when updating
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
