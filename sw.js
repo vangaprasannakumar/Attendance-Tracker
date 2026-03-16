7ab53ed5c50cd49787b22304a058c6063a3698f6
Prasanna Kumar Vanga <113172515+vangaprasannakumar@users.noreply.github.com>
2026-03-16 12:12:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
 const CACHE_NAME = 'capco-hrms-v2';
 
-// Install the Service Worker and cache the core files
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json'
-            ]);
+            return cache.addAll(['./', './index.html', './manifest.json']);
         })
     );
 });
 
-// Intercept network requests and serve from cache if offline
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith( caches.match(event.request).then((response) => { return response || fetch(event.request); }) );
 });
 
-// Clean up old caches when updating
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+    event.waitUntil( caches.keys().then((cacheNames) => { return Promise.all( cacheNames.map((cacheName) => { if (cacheName !== CACHE_NAME) return caches.delete(cacheName); }) ); }) );
 });
